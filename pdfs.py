30bb33dc279a303410a6112051692c64b0ee8001
Forest Gregg <fgregg@uchicago.edu>
2016-12-09 15:22:47 -0600

    download the things

---
+++ pdfs.py
@@ -0,0 +1,17 @@
+import csv
+import requests
+
+with open('CPSLeadTestingMap.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        filename = row[6]
+        url = 'http://cps.edu/SiteCollectionDocuments/LeadTesting/' + filename
+        response = requests.get(url)
+        with open(filename, 'wb') as outfile:
+            outfile.write(response.content)
+
+        break
+        
+        
+
