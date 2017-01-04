c9dda735bf47db7abeeb98abb769ae661c6c815e
Jean Cochrane <jean@jeancochrane.com>
2017-01-04 09:21:26 -0600

    Scripts file handles more patterns and exceptions

--- scripts/validate.py
+++ scripts/validate.py
@@ -3,14 +3,23 @@ import csv
 import re
 import datetime
 
-def clean(row, patterns):
-    for pattern in patterns:
-        try:
-            return pattern(row)
-        except:
-            continue
+def clean(row, patterns, force=False):
+    if not force:
+        for pattern in patterns:
+            try:
+                return pattern(row)
+            except:
+                continue
+        else:
+            pattern(row)
     else:
-        pattern(row)
+        for pattern in patterns:
+            try:
+                return pattern(row, force=True)
+            except:
+                continue
+        else:
+            pattern(row)
 
 def school_source(group):
     pdf = group.replace('.csv', '')
@@ -20,35 +29,90 @@ def school_source(group):
     source = 'http://www.cps.edu/SiteCollectionDocuments/LeadTesting/' + pdf
     return school, source
 
-def validate(group, sample, location, date, test_result):
+def validate(group, sample, location, date, test_result, force=False):
     assert re.match(r'^[\w\d/-]+$', sample) is not None
     school, source = school_source(group)
-    return (school, sample, location, to_time(date, DATE_FORMATS), to_result(test_result), source)
-            
-def pattern_1(row):
+    if not force:
+        return (school, sample, location, to_time(date, DATE_FORMATS), to_result(test_result), source)
+    else:
+        return (school, sample, location, to_time(date, DATE_FORMATS), to_result(test_result, force=True), source)      
+
+
+def pattern_1(row, force=False):
     group, sample, location, date, test_result, *rest = row
-    return validate(group, sample, location, date, test_result)
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
 
-def pattern_2(row):
+def pattern_2(row, force=False):
     group, sample, location, _, date, *mid, test_result, _ = row
-    return validate(group, sample, location, date, test_result)    
+    if not force:
+        return validate(group, sample, location, date, test_result)  
+    else:
+        return validate(group, sample, location, date, test_result, force=True)  
 
-def pattern_3(row):
+def pattern_3(row, force=False):
     group, sample, location, _, date, test_result, *rest = row
-    return validate(group, sample, location, date, test_result)
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
 
-def pattern_4(row):
+def pattern_4(row, force=False):
     group, _, _, sample, location, _, date, *mid, test_result, _ = row
-    return validate(group, sample, location, date, test_result)
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
 
-def pattern_5(row):
+def pattern_5(row, force=False):
     group, sample, location_date, test_result, *rest = row
     location, date = re.findall(r'^(.*)((?:1|2|3|4|5|6|7|8|9|10|11|12)/\d{1,2}/\d{1,4}.*)', location_date)[0]
-    return validate(group, sample, location, date, test_result)
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
+
+def pattern_6(row, force=False):
+    group, facility_id, sample, location, date, test_result, test_result_2 = row
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
+
+def pattern_7(row, force=False):
+    group, short_school, facility_id, sample, location, date, test_result, *rest = row
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
+
+def pattern_8(row, force=False):
+    group, sample, location, date, test_result, test_result_2 = row
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
+
+def pattern_9(row, force=False):
+    group, sample, location, date, time_in_pipe, test_result, test_result_2 = row
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
+
+def pattern_10(row, force=False):
+    group, short_school, facility_id, sample, location, date, time, test_result, *rest = row
+    if not force:
+        return validate(group, sample, location, date, test_result)
+    else:
+        return validate(group, sample, location, date, test_result, force=True)
 
 def to_time(date_string, date_formats):
     date_string = date_string.replace(' -\xad‐ ', ' ')
     date_string = date_string.replace(' - ', ' ')
+    date_string = date_string.replace(']', '')
     for date_format in date_formats:
         try:
             return datetime.datetime.strptime(date_string, date_format)
@@ -57,14 +121,21 @@ def to_time(date_string, date_formats):
     else:
         return datetime.datetime.strptime(date_string, date_format)            
 
-def to_result(test_result):
-    if test_result in {'ND', 'None Detected', 'None Detecetd'}:
+def to_result(test_result, force=False):
+    if test_result in {'ND', 'None Detected', 'None Detecetd', 'NT'}:
         return 'None Detected'
     elif test_result.startswith('<'):
         return test_result
-    elif test_result == '':
+    elif test_result in {'', 'Sample Not\nSubmitted'}:
         return None
-    return float(test_result)
+
+    if force:
+        return float(test_result)
+    else:
+        if float(test_result) > 400:
+            raise ValueError
+        else:
+            return float(test_result)
 
 def clean_sample_id(sample_id) :
     sample_id = sample_id.replace('-­‐', '-')
@@ -85,6 +156,8 @@ HEADER_FIELDS = {'Sample Collection', 'Time & Date', 'Test Results (ppb)',
                  'Sample ID # Sample Location Sample Collection Time & Date',
                  'remained in pipes Sample ID # Sample Collection Time & Test ResultsDate', 'ppb LevelTime & DateTest Results (ppb)'}
 
+CSV_HEADER = ['school', 'sample', 'location', 'date', 'result', 'pdf']
+
 DATE_FORMATS =  ('%m/%d/%y %I:%M %p',
                  '%m/%d/%Y %I:%M %p',
                  '%m/%d/%y %I:%M%p',
@@ -93,13 +166,17 @@ DATE_FORMATS =  ('%m/%d/%y %I:%M %p',
                  '%m/%d/%Y %I%p',
                  '%m/%d %I:%M',
                  '%m/%d/%y',
+                 '%I:%M%p',
+                 '%I:%M%P',
                  '%I:%M %p %m/%d/%y',
-                 '%I:%M%p %m/%d/%y')
+                 '%I:%M%p %m/%d/%y',
+                 '%I:%M%p %m/%d//%y')
 
 reader = csv.reader(sys.stdin)
 next(reader)
 
 out_writer = csv.writer(sys.stdout)
+out_writer.writerow(CSV_HEADER)
 
 with open('err.csv', 'w') as err_file:
     err_writer = csv.writer(err_file)
@@ -113,15 +190,56 @@ with open('err.csv', 'w') as err_file:
                                             pattern_2,
                                             pattern_3,
                                             pattern_4,
-                                            pattern_5)))
+                                            pattern_5,
+                                            pattern_6,
+                                            pattern_7,
+                                            pattern_8,
+                                            pattern_9,
+                                            pattern_10)))
         except:
             if (len(row) < 5
                 or re.match(r'^[\w\d/-]+$', row[1]) is None
-                or row[0] in {'Individualschool_Ravenswood_610141.pdf.csv'}):
+                or row[0] in [
+                    'IndividualSchool_Libby_610037.pdf.csv',
+                    'Individualschool_Ravenswood_610141.pdf.csv',
+                    ]):
                 school, source = school_source(row[0])
                 row[0] = school
                 row.append(source)
                 err_writer.writerow(row)
+            elif row[0] in [
+                # Schools with confirmed lead counts higher than 500
+                'IndividualSchool_Wells_610110.pdf.csv',
+                'Individualschool_Beasley_610246.pdf.csv',
+                'IndividualSchool_Hamline_609964.pdf.csv',
+                'IndividualSchool_Kershaw_610019.pdf.csv',
+                'Individualschool_Blair_610087.pdf.csv',
+                'Individualschool_Bouchet_609815.pdf.csv',
+                'Individualschool_Mireles_610171.pdf.csv',
+                'Individualschool_Onahan_610104.pdf.csv',
+                'Individualschool_Sherman_610172.pdf.csv',
+                'IndividualSchool_Pullman_610139.pdf.csv',
+                'IndividualSchool_Orr_610389.pdf.csv'
+            ]:
+                try:
+                    out_writer.writerow(clean(row, (pattern_1,
+                                                    pattern_2,
+                                                    pattern_3,
+                                                    pattern_4,
+                                                    pattern_5,
+                                                    pattern_6,
+                                                    pattern_7,
+                                                    pattern_8,
+                                                    pattern_9,
+                                                    pattern_10),
+                                              force=True))
+                except:
+                    if (len(row) < 5
+                        or re.match(r'^[\w\d/-]+$', row[1]) is None):
+                        school, source = school_source(row[0])
+                        row[0] = school
+                        row.append(source)
+                        err_writer.writerow(row)
             else:
                 print(row, file=sys.stderr)
                 raise
